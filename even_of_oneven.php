6d82ad0f7c31d6098852932a99fbb126eb8ae604
Captaincrogs <2003829@talnet.nl>
2019-11-14 12:02:37 +0100

    Uitwerking van de exercise

---
+++ even_of_oneven.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+
+<?php
+
+
+echo "wat is je nummer?  ";
+ $nummer = readline("");
+  if($nummer % 2 == 0) {  
+   echo "dit is een even getal";
+}
+
+
+
+else{
+ echo "dit nummer is oneven";
+}
+
+?>
+
